0c362c5323c83c1b0f4082fa07319e54a50db15d
THURRnado <arthurvieira.trabalho@gmail.com>
2025-08-21 10:54:31 -0300

    feat: adicionando respostas simples em json com dados do objeto por pk

--- objetos/urls.py
+++ objetos/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path("api/objeto/<int:pk>/", views.dados_json_objeto, name="dados_json_objeto"),
 ]
--- objetos/views.py
+++ objetos/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import JsonResponse, Http404
+from .models import Objeto
 
 # Create your views here.
+def dados_json_objeto(request, pk):
+
+    try:
+        objeto = Objeto.objects.get(pk=pk)
+    except Objeto.DoesNotExist:
+        raise Http404("Objeto não encontrado")
+
+    data = {
+        "id": objeto.id,
+        "name": objeto.name,
+        "description": objeto.description,
+        "image": objeto.img_object.url if objeto.img_object else None,
+        "user": objeto.user.username,
+        "dt_object": objeto.dt_object.strftime("%Y-%m-%d"),
+        "dt_create": objeto.dt_create.strftime("%Y-%m-%d %H:%M:%S"),
+        "dt_modified": objeto.dt_modified.strftime("%Y-%m-%d %H:%M:%S"),
+    }
+
+    return JsonResponse(data, json_dumps_params={"ensure_ascii": False})
